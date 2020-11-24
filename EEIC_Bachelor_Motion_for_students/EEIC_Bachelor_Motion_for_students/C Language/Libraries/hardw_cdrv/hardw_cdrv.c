0f810e85cc964d538d4e63d59990ebc78334f15d
kanransha <kanransya.iqqb_io_o4@icloud.com>
2020-11-24 18:53:05 +0900

    delete unused functions

--- EEIC_Bachelor_Motion_for_students/EEIC_Bachelor_Motion_for_students/C Language/Libraries/hardw_cdrv/hardw_cdrv.c
+++ EEIC_Bachelor_Motion_for_students/EEIC_Bachelor_Motion_for_students/C Language/Libraries/hardw_cdrv/hardw_cdrv.c
@@ -33,24 +33,6 @@ Author:		Yui Shirato
 //#include "../../Include/ECATMap.h" //add ADin
 #include "hardw_cdrv.h"
 
-#define		TOIDA_Single	(3276.7)	//DA�|�[�g��10V�o�������Ƃ���32767�� MyGate3->Chan[0].Dac[axis] = (int)32767 << 16; �ɏ���
-void hardw_vref(double vref)
-{
-	volatile struct GateArray3  *MyGate3; // Gate3�p�\���̕ϐ��̒�`
-	MyGate3 = GetGate3MemPtr(0); // Acc24E3[0]�̃A�h���X��ݒ�
-
-	if (vref>5){
-      vref=5;
-	 } else if (vref<-5){
-      vref=-5;
-	 } else {
-	 vref = vref;
-	 }
-
-	MyGate3->Chan[0].Dac[0] = (int)(vref*TOIDA_Single) << 16; //32767=10V�Ȃ̂ŁCTOIDA_Single��p�����Ɣ��f����
-	
-}
-
 #define TRQ_MAX 5 // [Nm]
 #define MAX_VAL 32767
 #define TRQ_CONST 0.9800 // [Nm/V]
@@ -70,13 +52,3 @@ void hardw_trqref(double trqref)
 
 	MyGate3->Chan[0].Dac[0] = (int)(trqref / TRQ_CONST * MAX_VAL) << 16;
 }
-
-#define One_Circle_Num	(312500) //1 circle = 360 deg = 312500
-
-void hardw_angle_fromdeg_tonum(double deg, double *out){
-*out = One_Circle_Num /360.0 * deg;
-}
-
-void hardw_angle_fromnum_todeg(double num, double *out){
-*out = num / One_Circle_Num * 360.0;
-} 
